d871917e8f016a5a853c93a6e7e53cac783e9a44
Anusha <anushareddy29200@gmail.com>
2025-04-23 02:01:06 +0000

    adding test cases

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-def add(x, y):
-    return x + y
+# def add(x, y):
+#   return x + y
 
-var =
-result = add(1, 2)
-print(f"the sum is: {result}")
+# var =
+# result = add(1, 2)
+# print(f"the sum is: {result}")
+
+
+def toyou(x):
+    return f"hi {x}"
+
+
+def add(x):
+    return x + 1
+
+
+def subtract(x):
+    return x - 1
